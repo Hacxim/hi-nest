2de7bdd3527458d99c16c308dfa5f46e31c99ab0
Core <core@hacxim.com>
2022-02-23 23:48:14 +0900

    run NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
 
+// decorator
 @Module({
   imports: [MoviesModule],
   controllers: [AppController],
